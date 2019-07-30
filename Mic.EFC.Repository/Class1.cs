6fbf6ff9c8054e21767542c68abf2c2b57553fc7
Any <Any@ANY>
2019-07-30 12:22:41 +0400

    Create dll, add models and appContext

---
+++ Mic.EFC.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Mic.EFC.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ Mic.EFC.Repository/Models/Genders.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mic.EFC.Repository.Models
+{
+    class Genders
+    {
+    }
+}
